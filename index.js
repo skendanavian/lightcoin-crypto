bbc51bf7151a51dfee98b091c29e051e5a1710ed
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-25 19:51:24 +0000

    refactored with subclasses of Transaction class

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-let balance = 500.00;
 
-class Withdrawal {
 
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account
+
+  }
+
+
+
+}
+
+class Withdrawal extends Transaction {
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
+
+}
+
+class Deposit extends Transaction {
+
+  commit() {
+    this.account.balance += this.amount;
+  }
+
 
 }
 
@@ -18,12 +44,12 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+console.log(myAccount)
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Balance:' + myAccount.balance);
